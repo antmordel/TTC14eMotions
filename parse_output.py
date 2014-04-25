46d354c52f519d47c5e1875ea56fb6172872eff4
Antonio Moreno (MBP) <amoreno@lcc.uma.es>
2014-04-26 01:18:23 +0200

    .

---
+++ parse_output.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import sys
+from argparse import *
+
+argp = ArgumentParser(description='Takes a Maude output and parses it to a \"clean\" term.')
+argp.add_argument('-i', action='store', required=True, help='input file')
+argp.add_argument('-o', action='store', required=True, help='output file')
+argp.add_argument('--show-time', action='store_true', help='show the time of the Maude model parsed')
+argp.add_argument('--verbose', action='store_true', help='show information about the parsing')
+
+arguments = argp.parse_args()
+
+ifile = open(arguments.i, 'r')
+ofile = open(arguments.o, 'w')
+
+modelStarted = False
+for line in ifile:
+  if (modelStarted and not('Bye.' in line)):
+    ofile.write(line)
+  else:
+    if 'rewrite in' in line: # 1st line
+      if arguments.verbose:
+        print('   '+line[0:len(line)-2].replace('rewrite', 'Rewritten'))
+    elif 'rewrites:' in line: # 2nd line
+      time = int(line[line.find('(')+1:line.find('ms', line.find('('))])
+      time = time/1000.0
+      if arguments.verbose:
+        print('   Rewrites: '+line[line.find(' ')+1:line.find(' ', line.find(' ')+1)]
+              +' in time '+str(time)+'s.')
+      elif arguments.show_time:
+        print('   Time: '+str(time)+'s.')
+    elif 'result ' in line:
+      if 'result [' in line:
+        print('   Ouch! There was an error. The returned term has an error type (kind).')
+      else:
+        modelStarted = True
+        ofile.write(line[line.find(':')+2:])
+
+ifile.close()
+ofile.close()
--- run-task1.sh
+++ run-task1.sh
@@ -4,12 +4,23 @@
 # Contact: {amoreno,duran}@lcc.uma.es
 # Last modification: April 25th, 2014
 
-maude_call=$('maude');
-
 echo -e \
   " --------------------------------------------\n" \
   "--- Task 1: e-Motions based solution.    ---\n" \
   "--------------------------------------------\n";
 
-echo "\nTask 1: N = 2";
+echo " Task 1: N = 2";
+echo "   loading..."
 maude2.4 -no-banner MaudeExecution/Task1/loadAllFilesN2.maude > MaudeExecution/Task1/outputModel/out-n2.xmi.maude
+echo " Done!"
+echo " Starting parsing..."
+python parse_output.py -i MaudeExecution/Task1/outputModel/out-n2.xmi.maude -o MaudeExecution/Task1/outputModel/out-n2-parsed.xmi.maude --show-time
+echo " Success!"
+
+echo " Task 1: N = 2";
+echo "   loading..."
+maude2.4 -no-banner MaudeExecution/Task1/loadAllFilesN2.maude > MaudeExecution/Task1/outputModel/out-n2.xmi.maude
+echo " Done!"
+echo " Starting parsing..."
+python parse_output.py -i MaudeExecution/Task1/outputModel/out-n2.xmi.maude -o MaudeExecution/Task1/outputModel/out-n2-parsed.xmi.maude --show-time
+echo " Success!"
