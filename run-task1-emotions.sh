39f5e595de94605ee7faf448dd95066d024044fc
Antonio Moreno (MBP) <amoreno@lcc.uma.es>
2014-04-26 10:01:49 +0200

    test files added

--- run-task1-emotions.sh
+++ run-task1-emotions.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# @authors Antonio Moreno-Delgado and Francisco Duráns
+# @authors Antonio Moreno-Delgado and Francisco Durán
 # Contact: {amoreno,duran}@lcc.uma.es
-# Last modification: April 25th, 2014
-
+# Last modification: April 26th, 2014
 
 
 echo -e \
   " --------------------------------------------\n" \
-  "--- Task 1: e-Motions based solution.    ---\n" \
+  "--- Task 1: e-Motions-based solution.    ---\n" \
   "--------------------------------------------\n";
 
 echo " Task 1: N = 2";
@@ -33,4 +32,12 @@ maude2.4 -no-banner MaudeExecution/Task1/loadAllFilesN1000.maude > MaudeExecutio
 echo " Done!"
 echo " Starting parsing..."
 python parse_output.py -i MaudeExecution/Task1/outputModel/out-n1000.xmi.maude -o MaudeExecution/Task1/outputModel/out-n1000-parsed.xmi.maude --verbose
+echo " Success!"
+
+echo " Task 1: N = 2000";
+echo "   loading..."
+maude2.4 -no-banner MaudeExecution/Task1/loadAllFilesN2000.maude > MaudeExecution/Task1/outputModel/out-n2000.xmi.maude
+echo " Done!"
+echo " Starting parsing..."
+python parse_output.py -i MaudeExecution/Task1/outputModel/out-n2000.xmi.maude -o MaudeExecution/Task1/outputModel/out-n2000-parsed.xmi.maude --verbose
 echo " Success!"
